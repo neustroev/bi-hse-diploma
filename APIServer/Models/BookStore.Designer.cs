88e935413c5873b0a2770418449cc2e57b4c472a
roman <paradise.develop@gmail.com>
2013-05-26 13:11:09 +0600

    initial web api commit #2

---
+++ APIServer/Models/BookStore.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'D:\github\bi-hse-diploma\APIServer\Models\BookStore.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
